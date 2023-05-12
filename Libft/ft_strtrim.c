9c94ca522a13345e28dc1b91b8d10a129a2ec388
Adricarmona <129745244+Adricarmona@users.noreply.github.com>
2023-05-12 16:08:36 +0200

    pa paco

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,39 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acarmona <acarmona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/06 22:40:47 by acarmona          #+#    #+#             */
-/*   Updated: 2023/05/06 22:40:47 by acarmona         ###   ########.fr       */
+/*   Created: 2023/05/08 18:23:35 by acarmona          #+#    #+#             */
+/*   Updated: 2023/05/08 18:23:35 by acarmona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-char * ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
 {
+char	*tmp;
 int	i;
 int	j;
-char *tmp;
 
-i = ft_strlen(s1);
-j = ft_strlen(s2);
-if (i == 0 && j == 0)
-	return (NULL);
-j = j + i;
+tmp = 0;
 i = 0;
-tmp = (char*)malloc(sizeof(char)*j);
-j = 0;
-while (s1[i] != '\0' && s2[j] != '\0')
+j = ft_strlen(s1);
+if (s1 != 0 && set != 0)
 {
-	if (s1[i] != '\0')
-		tmp[i] = s1[i];
-	else if (s2[j++] != '\0')
-		tmp[i] = s2[j];
-	i++;
+	while (ft_strchr(set,s1[i]) != 0)
+		i++;
+	while (ft_strchr(set,s1[j]) != 0 && j > i)
+		j--;
+	tmp = (char *)malloc(sizeof(char) * j - i);
+	ft_strlcpy(tmp, s1[i], j - i);	
 }
-return (tmp);
+return tmp;
 }
